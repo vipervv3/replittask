37da1081944a311721a3180a9320bb77ee110e52
vipervv3 <37425492+vipervv3@users.noreply.github.com>
2025-09-01 22:53:31 -0400

    Create vite.config.vercel.js

---
+++ api/vite.config.vercel.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "client", "src"),
+      "@shared": path.resolve(process.cwd(), "shared"),
+      "@assets": path.resolve(process.cwd(), "attached_assets"),
+    },
+  },
+  root: path.resolve(process.cwd(), "client"),
+  build: {
+    outDir: path.resolve(process.cwd(), "dist/public"),
+    emptyOutDir: true,
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
